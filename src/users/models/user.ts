5e081818248d18a03903c6724d712b2e0a107b71
haiz2k22 <khuat031@gmail.com>
2022-11-02 11:55:27 +0700

    second commit

---
+++ src/users/models/user.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field()
+  userId: string;
+  @Field()
+  email: string;
+  @Field(() => Int)
+  age: number;
+  @Field({ nullable: true })
+  isSubscribed?: boolean;
+}
